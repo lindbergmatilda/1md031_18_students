81005bc47bb85b96a8b7fe7454a6ee5e68b23aa8
lindbergmatilda <matilda.lindberg97@hotmail.com>
2020-11-17 09:22:47 +0100

    5 different burgers

---
+++ public/js/menu.js
@@ -0,0 +1,50 @@
+
+
+var food = [
+  {
+    burgerID: "happy",
+    name:"Super Happy Meal",
+    kCal: 880,
+    gluten: false,
+    lactose: false,
+    nuts: false,
+    imgUrl: "https://www.mcdonalds.com/is/image/content/dam/uk/nfl/nutrition/nfl-product/product/products/mcdonalds-Cheeseburger.jpg?$Product_Desktop$"
+  },
+  {
+    burgerID: "tasty",
+    name: "Super Tasty Meal",
+    kCal: 1100,
+    gluten: true,
+    lactose: true,
+    nuts: true,
+    imgUrl: "https://www.mcdonalds.com/is/image/content/dam/uk/nfl/nutrition/nfl-product/product/products/mcdonalds-Quarter-Pounder-with-Cheese.jpg?$Product_Desktop$"
+  },
+  {
+    burgerID:"double",
+    name:"Double Tasty Meal",
+    kCal:1500,
+    gluten:true,
+    lactose:true,
+    nuts:false,
+    imgUrl: "https://www.mcdonalds.com/is/image/content/dam/uk/nfl/nutrition/nfl-product/product/products/mcdonalds-Big-Mac.jpg?$Product_Desktop$"
+
+  },
+  {
+    burgerID:"no",
+    name:"No Meat",
+    kCal:10,
+    gluten:true,
+    lactose:false,
+    nuts:false,
+    imgUrl:"https://previews.123rf.com/images/ha4ipuri/ha4ipuri1806/ha4ipuri180600022/103956150-hamburger-bread-or-bun-without-anything-isolated-on-white-background-clipping-path.jpg"
+  },
+  {
+    burgerID:"bad",
+    name:"Bad Meal",
+    kCal:2500,
+    gluen:false,
+    lactose:true,
+    nuts:false,
+    imgUrl:"https://d2gg9evh47fn9z.cloudfront.net/800px_COLOURBOX2849655.jpg"
+  },
+]
